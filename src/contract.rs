fe7edc713d6a41f0392d809ed79a3b95fa8e7dd3
Hitisha-G <hitisha.gohil@ssipmt.com>
2024-04-26 19:52:47 +0530

    contract state-admins

--- src/contract.rs
+++ src/contract.rs
@@ -1,15 +1,26 @@
+use std::sync::Arc;
+
 use cosmwasm_std::{
-     to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult
+    to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
 
-use crate::msg::{GreetResp, QueryMsg};
+use crate::msg::{self, GreetResp, InstantiateMsg, QueryMsg};
+use crate::state::ADMINS;
 
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info : MessageInfo,
-    _msg: Empty,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> StdResult<Response> {
+    let admins: StdResult<Vec<_>> = msg
+    .admins
+    .into_iter()
+    .map(|addr| deps.api.addr_validate(&addr))
+    .collect();
+
+    ADMINS.save(deps.storage, &admins?)?;
+
     Ok(Response::new())
 }
 
@@ -25,19 +36,11 @@ pub fn instantiate(
 //     to_json_binary(&resp)
 // }
 
-pub fn query(
-    _deps : Deps,
-    _env : Env,
-    msg: QueryMsg
-) -> StdResult<Binary> {
+pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     use QueryMsg::*;
 
     match msg {
-        Greet {  } => {
-           
-            to_json_binary(&query::greet()?)
-        }
-        
+        Greet {} => to_json_binary(&query::greet()?),
     }
 }
 
@@ -45,11 +48,49 @@ mod query {
     use super::*;
 
     pub fn greet() -> StdResult<GreetResp> {
-        let resp = GreetResp{
-            message : "Hello CosmWasm".to_owned(),
+        let resp = GreetResp {
+            message: "Hello CosmWasm".to_owned(),
         };
 
         Ok(resp)
         // to_json_binary(&resp)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::from_json;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+
+    #[test]
+    fn greet_query() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+
+        instantiate(deps.as_mut(), env, mock_info("sender", &[]), InstantiateMsg{admins: vec![]}).unwrap();
+
+        let resp = query(deps.as_ref(), mock_env(), QueryMsg::Greet {}).unwrap();
+
+        let resp: GreetResp = from_json(&resp).unwrap();
+
+        assert_eq!(
+            resp,
+            GreetResp {
+                message: "Hello CosmWasm".to_owned(),
+            }
+        );
+    }
+    #[test]
+    fn greet_is_working() {
+        let resp = query::greet().unwrap();
+
+        assert_ne!(
+            resp,
+            GreetResp {
+                message: "not me".to_owned(),
+            }
+        )
     }
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,10 +1,17 @@
 use serde:: { Serialize, Deserialize};
 
-#[derive(Serialize,Deserialize)]
+#[derive(Serialize,Deserialize,PartialEq,Debug,Clone)]
 pub struct GreetResp {
     pub message: String,
 }
 
+
+#[derive(Serialize,Deserialize,PartialEq,Debug,Clone)]
 pub enum QueryMsg {
     Greet {},
+}
+
+#[derive(Serialize,Deserialize,PartialEq,Debug,Clone)]
+pub struct InstantiateMsg{
+    pub admins: Vec<String>,
 }
